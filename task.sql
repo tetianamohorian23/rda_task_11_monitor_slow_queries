44e3a3ada3ad71081a48ba4d0d0cebc0c3f77a9c
Tetiana Mohorian <tetiana.mohorian@student.tuke.sk>
2026-08-28 14:05:14 +0200

    Solution

--- task.sql
+++ task.sql
@@ -22,11 +22,6 @@ CREATE TABLE Products1 (
     PRIMARY KEY (ID)
 ) ENGINE=InnoDB;
 
-CREATE TABLE Products2 (
-    ID INT AUTO_INCREMENT,
-    Name VARCHAR(50),
-    PRIMARY KEY (ID)
-) ENGINE=MyISAM;
 
 -- Create test data in Products1 table 
 INSERT INTO Products1 (Name)
